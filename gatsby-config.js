6291d8d7d4098581e558dbf22e31bc096609023f
yuxuefu@live.cn <yuxuefu@live.cn>
2023-07-28 20:26:47 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,18 +4,18 @@ module.exports = {
   pathPrefix: process.env.PATH_PREFIX || '/',
   siteMetadata: {
     siteTitle: 'Workouts Map',
-    siteUrl: 'https://ben29.xyz',
-    logo: 'https://avatars3.githubusercontent.com/u/6956444?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
+    siteUrl: 'https://mytrack.run',
+    logo: 'https://img1.imgtp.com/2023/07/23/xdyH1D7i.png',
     description: 'Personal Workouts Page',
     keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
     navLinks: [
       {
-        name: 'Blog',
-        url: 'https://ben29.xyz',
+        name: 'Strava',
+        url: 'https://www.strava.com/athletes/34449231',
       },
       {
-        name: 'About',
-        url: 'https://github.com/ben-29/workouts_page/blob/master/README-CN.md',
+        name: 'SmashRun',
+        url: 'https://zh-tw.smashrun.com/yuxuefu/run/34630160',
       },
     ],
   },
